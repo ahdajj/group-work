5b0f5c9100615f503e9aeb8eae4507ac4cbd98f2
ahdajj <108350880+ahdajj@users.noreply.github.com>
2023-09-11 15:13:27 +0200

    Update commentModule.js

---
+++ models/commentModule.js
@@ -0,0 +1,31 @@
+const moment = require("moment/moment")
+const mongoose = require("mongoose")
+
+
+const Schema = mongoose.Schema ;
+const CommentSchema = new Schema ({
+    body:{
+        type:String,
+        required: true
+    },
+    PostId:{
+        type:mongoose.Types.ObjectId,
+        ref:'Post',
+        required:true
+    },
+    userId:{
+        type:mongoose.Types.ObjectId,
+        ref:'user',
+        required:true
+      },
+    created_at:{
+        type: Date ,
+        default :Date.now,
+        get:function(createAt){
+            return moment(createAt).format("DD/MM/YYYY ")
+        }
+    }
+})
+
+const Comment = mongoose.model('Comment' , CommentSchema)
+module.exports =  Comment;
