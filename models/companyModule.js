d2167ee4f91423b240fb442f1eebfb4631c3982e
ESSA <eng.bashar.alessa@gmail.com>
2023-09-15 15:17:43 +0200

    company

---
+++ models/companyModule.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema ;
+const companySchema = new Schema({
+       Name:{
+        type:String,
+        required: true
+       },
+       Email:{
+        type:String,
+        required: true
+       },
+       Password:{
+        type:String,
+        required: true
+       },
+       comment:[{
+              type:mongoose.Types.ObjectId,           
+              ref:'Comment'                           
+       }],
+
+})
+
+const  User = mongoose.model('Companies', comapnySchema)
+module.exports = User
